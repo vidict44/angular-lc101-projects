6a34d9b6c24fa6774a39cbea25707290df786e6a
Tanja Vidic <vidict44@gmail.comgit config --global user.name Tanja>
2019-10-24 08:34:00 -0700

    part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://media.boingboing.net/wp-content/uploads/2018/07/Wednesday-Addams.jpg';
+  image3 = 'https://www.thenavagepatch.com/wp-content/uploads/2017/09/Cousin-Itt-Halloween-prop-F01.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'Lord of the Rings', '3 Iron'];
 
    constructor() { }
 
