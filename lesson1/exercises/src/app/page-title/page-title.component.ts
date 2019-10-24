cec029877abac7d8280b3d4e989f60fb0e932e46
Tanja Vidic <vidict44@gmail.comgit config --global user.name Tanja>
2019-10-24 09:29:08 -0700

    part 3 complete

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-  title = "once";
+  title = "page title";
   constructor() { }
 
   ngOnInit() {
